be54080a8d99b986ec8526ee45e61b4b5dc24548
ManuelaCorte <manu.cortepause@gmail.com>
2023-08-02 17:44:52 +0200

    Initial commit

---
+++ src/test/java/com/sde/project/fileservice/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sde.project.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
